a3057d771a0ad888c08869c5d6f13acd12e2c4cb
Sergi Villalobos Gascón <56490583+Sergidev@users.noreply.github.com>
2026-05-25 10:43:50 +0000

    exercise corrected

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-// This is my function that sums two numbers
-const sum = (a,b) => {
-    return a + b
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
 }
 
-const fromEuroToDollar = (valueInEuro) => {
-return valueInEuro * 1.07
+const fromEuroToDollar = (euros) => {
+    return euros * oneEuroIs["USD"];
 }
 
-const fromDollarToEuro = (valueInDollar) => {
-return valueInDollar / 1.07
+const fromDollarToYen = (dollars) => {
+    return (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
 }
 
-const fromYensToEuros = (valueInYen) => {
-return valueInYen / 156.5
+const fromYenToPound = (yens) => {
+    return (yens / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
 }
-
-// Just a console log for ourselves
-console.log(sum(7,3))
 
 // Export the function to be used on other files 
 // (similar to the keyword "export" when using webpack)
-module.exports = { sum, fromEuroToDollar, fromYensToEuros, fromDollarToEuro };
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- test.js
+++ test.js
@@ -1,30 +1,14 @@
-// Import the function sum from the app.js file
-const { sum, fromDollarToEuro, fromYensToEuros } = require('./app.js');
-const { fromEuroToDollar } = require('./app.js');
-
-// Start your first test
-test('adds 14 + 9 to equal 23', () => {
-    // Inside the test we call our sum function with 2 numbers
-    let total = sum(14, 9);
-
-    // We expect the sum of those 2 numbers to be 23
-    expect(total).toBe(23);
-});
+// Import the functions from the app.js file
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
 test("1 euro ==> 1.07 dollars", function() {
-    const dollars = fromEuroToDollar(3.5);
-    const expected = 3.5 * 1.07; 
-    expect(dollars.toFixed(2)).toBe("3.75");
+    expect(fromEuroToDollar(3.5)).toBe(3.745);
 })
 
-test("1 dollar ==> 146.26 yens", function() {
-    const euros = fromDollarToEuro(1);
-    const expected = euros * 156.5; 
-    expect(expected.toFixed(2)).toBe("146.26");
+test("10 dollars ==> 1490.3 yens", function() {
+    expect(fromDollarToYen(10)).toBe(1462.6168224299065);
 })
 
-test("1,000 yens ==> 5.56 pounds", function() {
-    const euros = fromYensToEuros(1000);
-    const expected = euros * 0.87; 
-    expect(expected.toFixed(2)).toBe("5.56");
+test("1,000 yens ==> 7.72 pounds", function() {
+    expect(fromYenToPound(1000)).toBe(5.559105431309905);
 })
